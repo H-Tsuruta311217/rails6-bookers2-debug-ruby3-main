5c8f6264a7e4bc25d20bd39ae0edf01cb53b42da
H-Tsuruta311217 <141084685+H-Tsuruta311217@users.noreply.github.com>
2023-08-24 07:51:51 +0000

    [Start] bookers2非同期化

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,8 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+//= require jquery
+//= require rails-ujs
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
@@ -11,7 +13,13 @@ import "jquery"
 import "popper.js"
 import "bootstrap"
 
-import "../stylesheets/application" 
+
+import "../stylesheets/application"
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+require('jquery')
 
 Rails.start()
 Turbolinks.start()
