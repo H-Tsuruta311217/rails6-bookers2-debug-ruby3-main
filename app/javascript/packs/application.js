f882b3c1ea078720c8f598a14703109703a9ef2f
H-Tsuruta311217 <141084685+H-Tsuruta311217@users.noreply.github.com>
2023-09-02 04:10:18 +0000

    [Start] レビュー機能

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,3 +24,10 @@ require('jquery')
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import Raty from "raty.js"
+window.raty = function(elem,opt) {
+  let raty =  new Raty(elem,opt)
+  raty.init();
+  return raty;
+}
