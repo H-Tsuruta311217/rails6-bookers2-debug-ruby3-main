76f28ef17433e75c5f56dc1f048abd9fa565f22e
H-Tsuruta311217 <141084685+H-Tsuruta311217@users.noreply.github.com>
2023-09-04 07:11:28 +0000

    [Finish] 非同期通信化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,20 +2,18 @@ class BookCommentsController < ApplicationController
 
   def create
     @book = Book.find(params[:book_id])
-    @book_comment = current_user.book_comments.new(book_comment_params)
-    @book_comment.book_id = @book.id
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
     @user = @book.user
     @newbook = Book.new
-    if @book_comment.save
-      redirect_to book_path(@book), notice: "You have created comment successfully."
-    else
-      render 'books/show'
-    end
+    @comment.save
   end
 
   def destroy
-    BookComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
+    @comment = BookComment.find(params[:id])
+    @book = @comment.book
+    @comment.destroy
+    #redirect_to book_path(params[:book_id])
   end
 
   private
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,20 +15,18 @@ import "bootstrap"
 
 
 import "../stylesheets/application"
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-require('jquery')
+// require("@rails/activestorage").start()
+// require("channels")
+// require('jquery')
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-import Raty from "raty.js"
-window.raty = function(elem,opt) {
-  let raty =  new Raty(elem,opt)
-  raty.init();
-  return raty;
-}
-window.$ = window.jQuery = require('jquery');
+// import Raty from "raty.js"
+// window.raty = function(elem,opt) {
+//   let raty =  new Raty(elem,opt)
+//   raty.init();
+//   return raty;
+// }
+// window.$ = window.jQuery = require('jquery');
