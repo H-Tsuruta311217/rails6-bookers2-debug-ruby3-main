8c72f0b1b070e4d5c2e442d2e1f6accc56724ef6
H-Tsuruta311217 <141084685+H-Tsuruta311217@users.noreply.github.com>
2023-09-07 07:22:58 +0000

    [Add] グループ作成

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,6 @@ Rails.application.routes.draw do
   end
   resources :messages, only: [:create]
   resources :rooms, only: [:create,:show]
-
+  resources :groups, only: [:new, :index, :show, :create, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
