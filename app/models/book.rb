fce6dba811412958262a9e47b568b3e3a7024c23
H-Tsuruta311217 <141084685+H-Tsuruta311217@users.noreply.github.com>
2023-09-29 06:42:03 +0000

    [Add] 7日間の投稿数グラフ

--- app/models/book.rb
+++ app/models/book.rb
@@ -17,6 +17,11 @@ class Book < ApplicationRecord
   #今日1日で作成した 全Bookを取得
   scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
   #昨日1日で作成した 全Bookを取得
+  scope :created_2day_ago, -> { where(created_at: 2.day.ago.all_day) } # 2日前
+  scope :created_3day_ago, -> { where(created_at: 3.day.ago.all_day) } # 3日前
+  scope :created_4day_ago, -> { where(created_at: 4.day.ago.all_day) } # 4日前
+  scope :created_5day_ago, -> { where(created_at: 5.day.ago.all_day) } # 5日前
+  scope :created_6day_ago, -> { where(created_at: 6.day.ago.all_day) }
   scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) }
   #6日前の0:00から今日の23:59までに作成した 全Bookを取得
   scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) }
